a9912c922a020787bd49f534d806eef66cc4de64
domokoslaci94 <domokoslaci94@gmail.com>
2017-03-24 19:57:12 +0100

    Configured new plugin for sharing resources

--- generator/src/main/java/generator/ProjectGenerator.java
+++ generator/src/main/java/generator/ProjectGenerator.java
@@ -1,85 +1,89 @@
 package generator;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import representation.ClassRepresentation;
 import representation.ProjectRepresentation;
 import representation.operator.OperatorRepresentation;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ProjectGenerator {
 
-    private static final String GENERATED_UTILS_CONTENT_FILENAME = "GeneratedUtilsContent.txt";
-    private static final String GENERATED_UTILS_FILENAME = "GeneratedUtils";
+  private static final String GENERATED_UTILS_CONTENT_FILENAME = "GeneratedUtilsContent.txt";
+  private static final String GENERATED_UTILS_FILENAME = "GeneratedUtils";
 
-    StateGenerator stateGenerator = new StateGenerator();
-    OperatorGenerator operatorGenerator = new OperatorGenerator();
+  StateGenerator stateGenerator = new StateGenerator();
+  OperatorGenerator operatorGenerator = new OperatorGenerator();
 
-    public ProjectGenerator(StateGenerator stateGenerator,
-                            OperatorGenerator operatorGenerator) {
-        this.stateGenerator = stateGenerator;
-        this.operatorGenerator = operatorGenerator;
+  public ProjectGenerator(StateGenerator stateGenerator,
+      OperatorGenerator operatorGenerator) {
+    this.stateGenerator = stateGenerator;
+    this.operatorGenerator = operatorGenerator;
+  }
+
+  public List<ClassRepresentation> generate(ProjectRepresentation project, String directoryName,
+      String packageName)
+      throws IOException {
+
+    List<ClassRepresentation> result = new ArrayList<>();
+
+    stateGenerator.setState(project.getStateRepresentation());
+    stateGenerator.setKeepTogetherGettersAndSetters(true);
+    ClassRepresentation stateClass = stateGenerator
+        .generate(directoryName, packageName, project.getStateRepresentation().getName());
+    result.add(stateClass);
+
+    operatorGenerator.setStateClass(stateClass);
+
+    for (OperatorRepresentation operator : project.getOperatorRepresentation()) {
+      operatorGenerator.setOperator(operator);
+      ClassRepresentation operatorClass = operatorGenerator
+          .generate(directoryName, packageName, operator.getName());
+
+      result.add(operatorClass);
     }
 
-    public List<ClassRepresentation> generate(ProjectRepresentation project, String directoryName,
-                                              String packageName)
-            throws IOException {
+    ClassRepresentation generatedUtilsClassRepresentation = copyGeneratedUtils(directoryName,
+        packageName);
 
-        List<ClassRepresentation> result = new ArrayList<>();
+    result.add(generatedUtilsClassRepresentation);
+    return result;
+  }
 
-        stateGenerator.setState(project.getStateRepresentation());
-        stateGenerator.setKeepTogetherGettersAndSetters(true);
-        ClassRepresentation stateClass = stateGenerator
-                .generate(directoryName, packageName, project.getStateRepresentation().getName());
-        result.add(stateClass);
+  private ClassRepresentation copyGeneratedUtils(String directoryName, String packageName)
+      throws IOException {
+    ClassRepresentation generatedUtilsClassRepresentation = new ClassRepresentation();
+    generatedUtilsClassRepresentation
+        .setFilePath(
+            GeneratorUtils.getFilePath(directoryName, packageName, GENERATED_UTILS_FILENAME));
 
-        operatorGenerator.setStateClass(stateClass);
+    File resultFile = new File(
+        GeneratorUtils.getFilePath(directoryName, packageName, "GeneratedUtils"));
 
-        for (OperatorRepresentation operator : project.getOperatorRepresentation()) {
-            operatorGenerator.setOperator(operator);
-            ClassRepresentation operatorClass = operatorGenerator
-                    .generate(directoryName, packageName, operator.getName());
+    InputStream generatedUtilsContent = Thread.currentThread().getContextClassLoader()
+        .getResourceAsStream(GENERATED_UTILS_CONTENT_FILENAME);
 
-            result.add(operatorClass);
-        }
+    BufferedReader reader = new BufferedReader(new InputStreamReader(generatedUtilsContent));
+    BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
 
-        ClassRepresentation generatedUtilsClassRepresentation = copyGeneratedUtils(directoryName,
-                packageName);
+    String importLine = "package " + packageName + ";\n\n";
+    writer.append(importLine);
 
-        result.add(generatedUtilsClassRepresentation);
-        return result;
+    String currentLine;
+    while ((currentLine = reader.readLine()) != null) {
+      writer.append(currentLine);
+      writer.newLine();
     }
 
-    private ClassRepresentation copyGeneratedUtils(String directoryName, String packageName)
-            throws IOException {
-        ClassRepresentation generatedUtilsClassRepresentation = new ClassRepresentation();
-        generatedUtilsClassRepresentation
-                .setFilePath(GeneratorUtils.getFilePath(directoryName, packageName, GENERATED_UTILS_FILENAME));
+    writer.close();
+    reader.close();
 
-//    String generatedUtilsContentFile = Thread.currentThread().getContextClassLoader()
-//        .getResource(GENERATED_UTILS_CONTENT_FILENAME).getFile();
-
-        String generatedUtilsContentFile = "E:\\IntelliJ_Projects\\tdk2017\\visualizer\\GeneratedUtilsContent.txt";
-
-        File resultFile = new File(
-                GeneratorUtils.getFilePath(directoryName, packageName, "GeneratedUtils"));
-
-        BufferedReader reader = new BufferedReader(new FileReader(generatedUtilsContentFile));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(resultFile));
-
-        String importLine = "package " + packageName + ";\n\n";
-        writer.append(importLine);
-
-        String currentLine;
-        while ((currentLine = reader.readLine()) != null) {
-            writer.append(currentLine);
-            writer.newLine();
-        }
-
-        writer.close();
-        reader.close();
-
-        return generatedUtilsClassRepresentation;
-    }
+    return generatedUtilsClassRepresentation;
+  }
 }
